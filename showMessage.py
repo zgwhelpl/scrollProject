ec14d6706c17429816e7665b8ca36f0682ae9318
zgwhelpl <zgwhelpl@gmail.com>
2019-08-20 12:06:17 -0400

    redoing some init processes for showMessage

--- showMessage.py
+++ showMessage.py
@@ -18,36 +18,44 @@ def checkColor(RGB):
 	return check#'''
     
 def showMessage(text, **kwargs):
+
+    font = [255, 255, 255]
+    backGround = [0, 0, 0]
+    speed = .1
+    width = 8
+    BGimage = False
     if 'font' in kwargs:
-        t = kwargs.get('font')
+        font = kwargs.get('font')
         print("the font is " + str(t))
-    else :
-        t = [255, 255, 255]
-        print("no font set")
+    '''else :
+        font = [255, 255, 255]
+        print("no font set")#'''
     if 'backGround' in kwargs:
-        b = kwargs.get('backGround')
+        backGround = kwargs.get('backGround')
         print("the background is " + str(b))
-    else :
-        b = [0, 0, 0]
-        print("no BG set")
+    '''else :
+        backGround = [0, 0, 0]
+        print("no BG set")#'''
     if 'speed' in kwargs:
         speed = kwargs.get('speed')
-    else:
-        speed = .1
+    '''else:
+        speed = .1#'''
     if 'width' in kwargs:
         width = kwargs.get('width')
-    else :
-        width = 8
-    flagRaised = False
+    '''else :
+        width = 8#'''
+    
+
     if 'BGimage' in kwargs:
+        flagRaised = False
         if len(kwargs.get('BGimage')) == 64:
             for color in kwargs.get('BGimage'):
                 if not checkColor(color):
                     flagRaised = True
         if not flagRaised:
                 BGimage = kwargs.get('BGimage')
-        else:
-            BGimage = False
+        '''else:
+            BGimage = False#'''
                
     sen = sentence(text)
     
@@ -55,12 +63,12 @@ def showMessage(text, **kwargs):
         matrix = sen.frame(index) #this is the slide at that frame
         for inst in range(64): #for each part of the slide (all 64)
             if matrix[inst]: #they should be 1's and 0's, so if 1
-                matrix[inst] = [t[0], t[1], t[2]] #set that inst to t = [255, 255, 255]
+                matrix[inst] = [font[0], font[1], font[2]] #set that inst to t = [255, 255, 255]
             else : #otherwise its a 0
                 if (BGimage is not False):
                     matrix[inst] = BGimage[inst]
                 else:
-                    matrix[inst] = [b[0], b[1], b[2]] #so set it to b = [0, 0, 0] / background
+                    matrix[inst] = [backGround[0], backGround[1], backGround[2]] #so set it to b = [0, 0, 0] / background
                     #the final result should be [[],[],[],...x64]
         sense.set_pixels(matrix)#that type of matrix is feedable into set_pixels
         sleep(speed)
